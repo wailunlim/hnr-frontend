2999dd3f5de145334e5a47d648b3994641b8569b
kayheen <kayheen@u.nus.edu>
2020-01-19 09:56:59 +0800

    users can upload their own pictures

--- src/store/db.js
+++ src/store/db.js
@@ -1,20 +1,5 @@
-import { storage } from '../firebase/init.js'
-import firebase from 'firebase'
+import * as firebase from "firebase";
 
 export const db = firebase
     .initializeApp({ storageBucket: "samurai-e00ed.appspot.com" })
     .storage();
-
-export default {
-    methods: {
-        uploadFile(e) {
-            const file = e.target.files[0];
-            storage.ref('images/' + file.name).put(file)
-                .then(response => {
-                    response.ref.getDownloadURL().then((downloadURL) => {
-                        firebase.database().ref(YOUR_DATABASE).child(THE_USER_ID).update({ imageUrl: downloadURL })
-                    }                 
-					.catch(err => console.log(err))
-				}
-	}
-}
