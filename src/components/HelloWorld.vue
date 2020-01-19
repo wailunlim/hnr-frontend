ba22bef5f97a9f0008e7ae871fd3c5cd4abf99c7
kayheen <kayheen@u.nus.edu>
2020-01-19 12:01:08 +0800

    allow multiple upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,51 +1,86 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <input type="file" accept="image/jpeg,image/png" @change="uploadFile($event);">
-        <!--<b-form-input 
-           v-model="teamAPlayer"
-           placeholder="Enter player's name"></b-form-input>-->
-        <!--<button @click="uploadFile">Test</button>-->
-        <img src="../assets/samuraiLogo.png" />
-        <div>
-            <router-link to="/game">Play</router-link>
-        </div>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input
+      type="file"
+      accept="image/jpeg,image/png"
+      @change="uploadFile($event)"
+      multiple
+    />
+
+    <img src="../assets/samuraiLogo.png" />
+    <div>
+      <router-link to="/game">Play</router-link>
     </div>
+  </div>
 </template>
 
 <script>
-    //import storage from "../store/db.js";
-    import * as firebase from "firebase";
+//import storage from "../store/db.js";
+import * as firebase from "firebase";
+const axios = require("axios");
 
-    const db = firebase
-        .initializeApp({ storageBucket: "samurai-e00ed.appspot.com" })
-        .storage();
+const db = firebase
+  .initializeApp({ storageBucket: "samurai-e00ed.appspot.com" })
+  .storage();
 
-    export default {
-        name: "HelloWorld",
-        props: {
-            msg: String
-        },
-        mounted() {
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  mounted() {
     this.$store.dispatch("initialiseGame");
   },
-        methods: {
-            initialiseGamePlay() {
-                this.$store.dispatch("initialiseGame");
-            },
-            uploadFile(e) {
-                //console.log("upload file called with value");
-                const file = e.target.files[0];
-                //console.log(file);
-                db.ref('images/' + file.name).put(file)
-                    .then(response => {
-                        console.log(response)
-                    })
-                    .catch(err => console.log(err))
-            }
-        }
-    };
+  methods: {
+    initialiseGamePlay() {
+      this.$store.dispatch("initialiseGame");
+    },
+    async uploadFile(e) {
+      //console.log("upload file called with value");
+      //var length = e.files.length;
+      //console.log(length);
+      //const file = e.target.files[0];
+      //const file2 = e.target.files[1];
+      //console.log(file);
+      //console.log(file2);
+      var list = [];
+      for (var i = 0; i < 10; i++) {
+        var name = e.target.files[i].name;
+        console.log(name);
+        await db
+          .ref("images/" + name)
+          .put(e.target.files[i])
+          .then(response => {
+            console.log(response);
+          })
+          .then(async () => {
+            console.log(name);
+            var temp = await db.ref("images/" + name).getDownloadURL();
+            console.log("url is " + temp);
+            list.push(temp);
+            console.log(list);
+          })
+          .catch(err => console.log(err));
+      }
 
+      await axios({
+        method: "post",
+        url: "http://192.168.1.94:3000/sendimages",
+        data: {
+          url: list
+        }
+      })
+        .then(function(response) {
+          return response.data.tags;
+        })
+        .catch(function(error) {
+          console.log(error);
+          return -1;
+        });
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
